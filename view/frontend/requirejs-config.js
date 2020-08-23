4fb7f1e8777505d358771d63129f0dee44ad5e37
Vinit-github <vini.contact@gmail.com>
2020-08-23 12:40:16 +0530

    Final Commit

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,6 +3,8 @@ var config = {
         '*': {
             'AjaxCart': 'Xlab_AjaxCartQty/js/cartValueIncDec',
             'CartQtyUpdate': 'Xlab_AjaxCartQty/js/cartQtyUpdate',
+            'sidebar': 'Xlab_AjaxCartQty/js/sidebar',
+            'Magento_Checkout/js/view/minicart': 'Xlab_AjaxCartQty/js/view/minicart',
             'Magento_Checkout/template/minicart/item/default.html': 'Xlab_AjaxCartQty/template/minicart/item/default.html'
         }
     },
